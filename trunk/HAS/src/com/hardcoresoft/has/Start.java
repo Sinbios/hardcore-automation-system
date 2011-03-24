55b27c9077dfbb618749bbc4bff4cb7ee5229b84
andrei.tulai@gmail.com <andrei.tulai@gmail.com@a59c25e4-c386-647b-6adf-11d09d8edeb5>
2011-03-24 16:55:17 +0000

    test

--- trunk/HAS/src/com/hardcoresoft/has/Start.java
+++ trunk/HAS/src/com/hardcoresoft/has/Start.java
@@ -1,5 +1,7 @@
 package com.hardcoresoft.has;
 
+import org.apache.activemq.command.ActiveMQBlobMessage;
+
 public class Start {
 
 	/**
@@ -7,7 +9,7 @@ public class Start {
 	 */
 	public static void main(String[] args) {
 		// TODO init stuff
-
+		ActiveMQBlobMessage msg = new ActiveMQBlobMessage();
 	}
 
 }
