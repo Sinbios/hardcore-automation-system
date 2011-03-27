3add2809308fa37f4bb0a38608228f1112b9fedf
andrei.tulai@gmail.com <andrei.tulai@gmail.com@a59c25e4-c386-647b-6adf-11d09d8edeb5>
2011-03-27 06:01:32 +0000

    messaging base... HAS inits a listener attached to the test message queue, you can use component to send messages to this queue, the listener will be triggered and we display a message on the server console.

---
+++ HAS/src/com/hardcoresoft/has/messaging/HASMessageListener.java
@@ -0,0 +1,73 @@
+package com.hardcoresoft.has.messaging;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import com.hardcoresoft.has.util.MessageUtil;
+
+import javax.jms.*;
+
+public class HASMessageListener implements MessageListener {
+	private static HASMessageListener listener = new HASMessageListener();
+    private static int ackMode = Session.AUTO_ACKNOWLEDGE;
+    private static String messageQueueName = MessageUtil.getQueueName();
+
+    private Session session;
+    private boolean transacted = false;
+    private MessageProducer replyProducer;
+
+    public HASMessageListener() {
+        this.setupMessageQueueConsumer();
+    }
+
+    private void setupMessageQueueConsumer() {
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(ActiveMQConnection.DEFAULT_BROKER_URL);
+        Connection connection;
+        try {
+            connection = connectionFactory.createConnection();
+            connection.start();
+            this.session = connection.createSession(this.transacted, Session.AUTO_ACKNOWLEDGE);
+            Destination adminQueue = this.session.createQueue(MessageUtil.getQueueName());
+
+            //Setup a message producer to respond to messages from clients, we will get the destination
+            //to send to from the JMSReplyTo header field from a Message
+            this.replyProducer = this.session.createProducer(null);
+            this.replyProducer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+
+            //Set up a consumer to consume messages off of the admin queue
+            MessageConsumer consumer = this.session.createConsumer(adminQueue);
+            consumer.setMessageListener(this);
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onMessage(Message message) {
+        try {
+            TextMessage response = this.session.createTextMessage();
+            if (message instanceof TextMessage) {
+                TextMessage txtMsg = (TextMessage) message;
+                String messageText = txtMsg.getText();
+                System.out.println("MSG[" + messageText + "]");
+                response.setText(messageText);
+            }
+
+            //Set the correlation ID from the received message to be the correlation id of the response message
+            //this lets the client identify which message this is a response to if it has more than
+            //one outstanding message to the server
+            response.setJMSCorrelationID(message.getJMSCorrelationID());
+
+            //Send the response to the Destination specified by the JMSReplyTo field of the received message,
+            //this is presumably a temporary queue created by the client
+            
+            System.out.println("Server received message. MessageHandler would do work here.");
+            // Commenting this out for now since I don't want to reply.
+            //this.replyProducer.send(message.getJMSReplyTo(), response);
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+    public static MessageListener getInstance() {
+    	return listener;
+    }
+}
