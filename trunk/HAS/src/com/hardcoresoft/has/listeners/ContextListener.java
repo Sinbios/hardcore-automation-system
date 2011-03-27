6305b13ce1ceda700e1219ae879134c5df0e43dd
andrei.tulai@gmail.com <andrei.tulai@gmail.com@a59c25e4-c386-647b-6adf-11d09d8edeb5>
2011-03-27 01:24:05 +0000

    init

---
+++ trunk/HAS/src/com/hardcoresoft/has/listeners/ContextListener.java
@@ -0,0 +1,43 @@
+package com.hardcoresoft.has.listeners;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ * Application Lifecycle Listener implementation class ContextListener
+ *
+ */
+@WebListener
+public class ContextListener implements ServletContextListener {
+	  private ServletContext context = null;
+
+	  public ContextListener() {}
+
+	  //This method is invoked when the Web Application
+	  //has been removed and is no longer able to accept
+	  //requests
+
+	  public void contextDestroyed(ServletContextEvent event)
+	  {
+
+	    //Output a simple message to the server's console
+	    System.out.println("Catalina says: CYA LATER MAH NIGGUH!");
+	    this.context = null;
+
+	  }
+
+
+	  //This method is invoked when the Web Application
+	  //is ready to service requests
+
+	  public void contextInitialized(ServletContextEvent event)
+	  {
+	    this.context = event.getServletContext();
+
+	    //Output a simple message to the server's console
+	    System.out.println("Catalina says: SUP NIGGA, SERVER READY!");
+
+	  }
+}
