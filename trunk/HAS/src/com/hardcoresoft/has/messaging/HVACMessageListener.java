40e3b508803fc1bf95dea0fd86593576f7955656
michaelpbieniek@gmail.com <michaelpbieniek@gmail.com@a59c25e4-c386-647b-6adf-11d09d8edeb5>
2011-03-28 23:57:56 +0000

    Messaging updates.

--- trunk/HAS/src/com/hardcoresoft/has/messaging/HVACMessageListener.java
+++ trunk/HAS/src/com/hardcoresoft/has/messaging/HVACMessageListener.java
@@ -6,13 +6,14 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 import sun.jdbc.odbc.OdbcDef;
 
 import com.hardcoresoft.has.datastorage.DataStorage;
+import com.hardcoresoft.has.datastorage.HVACStatus;
 import com.hardcoresoft.has.util.MessageUtil;
 
 
 import javax.jms.*;
 
-public class HASMessageListener implements MessageListener {
-	private static HASMessageListener listener = new HASMessageListener();
+public class HVACMessageListener implements MessageListener {
+	private static HVACMessageListener listener = new HVACMessageListener();
     private static int ackMode = Session.AUTO_ACKNOWLEDGE;
     private static String messageQueueName = MessageUtil.getQueueName();
 
@@ -20,8 +21,8 @@ public class HASMessageListener implements MessageListener {
     private boolean transacted = false;
     private MessageProducer replyProducer;
     
-    public HASMessageListener() {
-        this.setupMessageQueueConsumer(DataStorage.getInstance().getoHVACData().getoHVACData().getsMsgQueueName());
+    public HVACMessageListener() {
+        this.setupMessageQueueConsumer("HVACQueue");
     }
 
     private void setupMessageQueueConsumer(String queuename) {
@@ -53,9 +54,11 @@ public class HASMessageListener implements MessageListener {
                 TextMessage txtMsg = (TextMessage) message;
                 String messageText = txtMsg.getText();
                 System.out.println("MSG[" + messageText + "]");
-                response.setText(messageText);
+                MessageHandler(messageText);
+                   
             }
-
+            
+    
             //Set the correlation ID from the received message to be the correlation id of the response message
             //this lets the client identify which message this is a response to if it has more than
             //one outstanding message to the server
@@ -71,6 +74,26 @@ public class HASMessageListener implements MessageListener {
             e.printStackTrace();
         }
     }
+    
+    private void MessageHandler(String message)
+    {
+    	if(message.equals("Connect")){
+    		DataStorage.getInstance().getoHVACData().getoHVACData().setbConnected(true);
+    		DataStorage.getInstance().updateHVACData();
+    	}
+    	else if(message.contains("CurrentTemperature")){
+    		System.out.println("Current Temperature is: "+message.split(":")[1]);
+    		try{
+    		HVACMessageSender.getInstance().sendMessage("hvac:test send");
+    		}
+    		catch(Exception e)
+    		{
+    			e.printStackTrace();
+    		}
+    	}
+    	
+    }
+    
     public static MessageListener getInstance() {
     	return listener;
     }
